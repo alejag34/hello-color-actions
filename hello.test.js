9386c0ab09eb30fd639708d0d498e02943ea6e26
alejag34 <alejag34@gmail.com>
2024-06-10 23:33:23 -0500

    1.2 cambio 2

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola mundo de Ale!");
+    expect(hello.hello()).toEqual("Hola mundo de AleG!");
   });
 });
